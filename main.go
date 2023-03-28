749811a8fa6817734ce95720be3effcfcdee0409
Ed Everett <ed@evere.tt>
2023-03-28 23:16:49 +0100

    I actually did something for once

--- main.go
+++ main.go
@@ -3,31 +3,45 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 )
 
 func main() {
 	// get url from user
-	url := flag.String("url", "", "URL to fetch")
+	url := flag.String("url", "https://example.com/", "URL to fetch")
 
 	flag.Parse()
 	// validate url
+	if url == nil {
+		fmt.Println("No URL provided")
+		return
+	}
+
+	if *url == "" {
+		fmt.Println("Invalid URL provided")
+		return
+	}
 
 	// fetch data
 	res, err := http.Get(*url)
 	if err != nil {
-		panic(err)
+		fmt.Println("Error whilst fetching: ", err)
+		return
 	}
 	defer res.Body.Close()
 
-	content, err := ioutil.ReadAll(res.Body)
+	content, err := io.ReadAll(res.Body)
 	if err != nil {
-		panic(err)
+		fmt.Println("Unable to ready body: ", err)
 
 	}
 
 	fmt.Println(string(content))
 
 	// output
+}
+
+func fetch() {
+
 }
